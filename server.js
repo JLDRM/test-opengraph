eb62a069699a7530829f02a15540d75999318981
joseplopezderecaldemartorell <josep.lopez.de.recalde.martorell@everis.com>
2019-12-18 18:26:55 +0100

    fix server configuration

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/test-opengraph'));
+app.use(express.static('./dist/test-opengraph'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/test-opengraph/index.html'));
+res.sendFile(path.join('./dist/test-opengraph/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
